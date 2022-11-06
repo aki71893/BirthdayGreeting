7c57229623946a79e63eb57313eb829854bb671c
aki71893 <nairakash898@gmail.com>
2022-11-06 14:04:25 +0530

    first commit

---
+++ app/src/main/java/com/example/buttday/BirthdayGreet.kt
@@ -0,0 +1,11 @@
+package com.example.buttday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greet)
+    }
+}
---
+++ app/src/main/java/com/example/buttday/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.buttday
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun CreateBirthdayCard(view: View) {
+
+
+
+
+
+        val intent = Intent(this, BirthdayGreet::class.java)
+
+        startActivity(intent)
+    }
+
+
+}
